9dc3eacb84e80030c8341c0c5b50ac48574fae21
Abdulaziz Alduhami <aziz.alduhami@gmail.com>
2018-07-11 00:54:58 +0300

    config changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,20 +2,20 @@ module.exports = {
   siteMetadata: {
     title: 'Thoag - تطبيق ذوق l تنسيق ولائم وحفلات',
   },
-  pathPrefix: "/thoag-test",
+  pathPrefix: "/thoag",
   plugins: ['gatsby-plugin-react-helmet',`gatsby-plugin-styled-components`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography.js`,
-      },
-    },
+    {resolve: `gatsby-plugin-typography`, options: {pathToConfigModule: `src/utils/typography.js`}},
     {
       resolve: `gatsby-plugin-intercom`,
       options: {
         appId: 'hdbiaxgw',
       },
     },
-  ],
-  
+    {
+      resolve: 'gatsby-plugin-intercom-spa',
+      options: {
+        app_id: 'hdbiaxgw'
+      }
+    }
+  ],  
 }
